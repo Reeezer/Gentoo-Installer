39264af0fc5ea5be153fb81bb6a8f9747bb229be
Léon Muller <leonmuller@hotmail.fr>
2022-03-25 08:54:29 +0100

    Refactored

--- generate_conf.py
+++ generate_conf.py
@@ -1,4 +1,4 @@
-class Config:
+class ConfigGenerator:
     def __init__(self, file, title, attributes):
         self.title = title
         self.file = file
@@ -16,16 +16,16 @@ if __name__ == '__main__':
     
     confs = list()
     
-    confs.append(Config(file1, 'gentoo', {'arch':'amd64', 'initsystem':'openrc'}))
-    confs.append(Config(file1, 'drive', {'drive':'/dev/sda', 'size':'32768', 'label':'gpt'}))
-    confs.append(Config(file1, 'mirror', {'url':'https://mirror.init7.net'}))
-    confs.append(Config(file1, 'portage', {'profile':'default/linux/amd64/17.1'}))
-    confs.append(Config(file1, 'timezone', {'profile':'Europe/Zurich'}))
-    confs.append(Config(file1, 'kernel', {'config':'distkernel', 'distkernel':'gentoo-kernel-bin'}))
+    confs.append(ConfigGenerator(file1, 'gentoo', {'arch':'amd64', 'initsystem':'openrc'}))
+    confs.append(ConfigGenerator(file1, 'drive', {'drive':'/dev/sda', 'size':'32768', 'label':'gpt'}))
+    confs.append(ConfigGenerator(file1, 'mirror', {'url':'https://mirror.init7.net'}))
+    confs.append(ConfigGenerator(file1, 'portage', {'profile':'default/linux/amd64/17.1'}))
+    confs.append(ConfigGenerator(file1, 'timezone', {'profile':'Europe/Zurich'}))
+    confs.append(ConfigGenerator(file1, 'kernel', {'config':'distkernel', 'distkernel':'gentoo-kernel-bin'}))
     
-    confs.append(Config(file2, 'general', {'unit':'MiB', 'drive':'/dev/sda'}))
-    confs.append(Config(file2, 'boot', {'start':'2', 'size':'200', 'mountpoint':'/boot', 'filesystem':'ext2'}))
-    confs.append(Config(file2, 'root', {'size':'8192', 'mountpoint':'/', 'filesystem':'ext2'}))
-    confs.append(Config(file2, 'home', {'mountpoint':'/home', 'filesystem':'ext2'}))
+    confs.append(ConfigGenerator(file2, 'general', {'unit':'MiB', 'drive':'/dev/sda'}))
+    confs.append(ConfigGenerator(file2, 'boot', {'start':'2', 'size':'200', 'mountpoint':'/boot', 'filesystem':'ext2'}))
+    confs.append(ConfigGenerator(file2, 'root', {'size':'8192', 'mountpoint':'/', 'filesystem':'ext2'}))
+    confs.append(ConfigGenerator(file2, 'home', {'mountpoint':'/home', 'filesystem':'ext2'}))
     
     [conf.generate() for conf in confs]
